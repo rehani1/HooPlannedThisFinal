120db119aa0d81f07e1c428d4630fce4163d4ba0
lolaquraishi <uev7gx@virginia.edu>
2025-08-25 17:25:13 -0400

    added home page connecting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login.jsx";
+import Home from ".pages/home.jsx";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} /> 
         <Route path="/dashboard" element={<h1>Dashboard (protected)</h1>} />
         <Route path="/request-account" element={<h1>Request Account</h1>} />
         <Route path="*" element={<Navigate to="/login" replace />} />
